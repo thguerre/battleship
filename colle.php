c09a6bc7cf6c13b3910e66e3487a7f11bec7d1da
Théo Guerre <guerre.theo@gmail.com>
2022-10-13 15:38:29 +0200

    add - step 3

--- colle.php
+++ colle.php
@@ -62,9 +62,86 @@ function colle (int $x, int $y, array $coords) : void
 		$updateGrid($grid, $coord[0], $coord[1], 'X');
 	}
 
-	$printGrid($x, $y, $grid, $tools);
-	//var_dump($grid);
+	// values for forecoming battleship
+	// ' ' = empty/missed
+	// 1/2 = a ship of player 1/2
+	//  X  = wreck of a ship
+	$validCoord = function ($str, $x, $y) : bool | array {
+		// validity check
+		$match = preg_match('/^ {0,}\[ {0,}[0-9]{1,} {0,}, {0,}[0-9]{1,} {0,}\] {0,}$/', $str);
+		if (!$match)
+			return false;
+		$coordsMatch = [];
+		preg_match_all('/[0-9]{1,}/', $str, $coordsMatch);
+		//var_dump($coordsMatch);
+		$mx = intval($coordsMatch[0][0]);
+		$my = intval($coordsMatch[0][1]);
+		//var_dump($mx, $my);
+		if ($mx > $x || $my > $y)
+			return false;
+		else
+			return [$mx, $my];
+		//return $match;
+		// recovering coords
+		// [0-9]{1,}
+	};
 
+	$printCoordsInvalid = function () {
+		echo "Coords have to be in [x, y] format, use `help` if you need to\n";
+	};
+	
+	//var_dump($validCoord(" [ 1 , 2 ] ", $x, $y));
+
+	$printGrid($x, $y, $grid, $tools);
+	while (true) {
+		echo "$> ";
+		$userEntry = readline();
+		//echo "you entered $userEntry\n";
+		$argStart = strpos($userEntry, ' ', 0);
+		$command = $argStart > 0 ? substr($userEntry, 0, $argStart) : substr($userEntry, 0);
+		//var_dump('command', $command, $argStart);
+		$coords = $validCoord(substr($userEntry, $argStart), $x, $y);
+		switch ($command) {
+			// add "help" and "exit" for bonus
+			case "query":
+				if (!$coords) {
+					$printCoordsInvalid();
+					break;
+				}
+				echo $grid[$coords[0]][$coords[1]] != ' ' ? "full\n" : "empty\n";
+				break;
+			case "add":
+				if (!$coords) {
+					$printCoordsInvalid();
+					break;
+				}
+
+				if ($grid[$coords[0]][$coords[1]] != ' ')
+					echo "A cross already exists at this location\n";
+				else
+					$grid[$coords[0]][$coords[1]] = 'X';
+				break;
+			case "remove":
+				if (!$coords) {
+					$printCoordsInvalid();
+					break;
+				}
+
+				if ($grid[$coords[0]][$coords[1]] == ' ')
+					echo "No cross exists at this location\n";
+				else
+					$grid[$coords[0]][$coords[1]] = ' ';
+				break;
+			case "display":
+				$printGrid($x, $y, $grid, $tools);
+				break;
+			default:
+				echo "Unknown command \"$command\"\n";
+				break;
+		}
+	}
+
+	
 }
 
 
